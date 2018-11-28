8e0d8833d6bd79108de65c8e851939a4066fa759
medbar <tonymit@yandex.ru>
2018-11-28 13:24:06 +0300

    end disc/lab1

--- 1sem/dis/1lab_cpp/main.cpp
+++ 1sem/dis/1lab_cpp/main.cpp
@@ -20,28 +20,34 @@ struct matrix{
 };
 
 
-template <typename T>
+//template <typename T>
 class block_write_queue{
-    std::queue<T> data;
+    std::queue<std::pair<int,int>> data;
     std::mutex w_lock;
+    //std::mutex r_tasks;
 
 public:
     bool done = false;
     bool have_tasks = false;
     block_write_queue() = default;
-    T pop(){
+    std::pair<int,int>* pop(){
         w_lock.lock();
-        T e = data.front();
+        if (data.empty()){
+            have_tasks = false;
+            w_lock.unlock();
+            return NULL;
+        }
+        std::pair<int,int> *e = new std::pair<int,int>(data.front());
         data.pop();
         have_tasks = not data.empty();
         w_lock.unlock();
         return e;
     };
 
-    void push(T e){
+    void push(std::pair<int,int> e){
         w_lock.lock();
         data.push(e);
-        have_tasks= true;
+        have_tasks = true;
         w_lock.unlock();
     }
 };
@@ -49,38 +55,39 @@ public:
 
 std::mutex out_lock;
 std::mutex all_matrix_lock;
-void anyFunc(block_write_queue<std::pair<int,int>> * q, std::vector<matrix*> *data_ptr, matrix * out_matrix, int shape) {
+void anyFunc(block_write_queue * q, std::vector<matrix*> *data_ptr, matrix * out_matrix, int shape) {
     std::vector<matrix*>& data =  *data_ptr;
 
     //std::cout<<"thread function started" << std::endl;
     while (true){
-        if (q->have_tasks){
-            //std::cout<<"thread start task" << std::endl;
-            std::pair<int,int> task = q->pop();
-            //matrix m[] = {data[task.first], data[task.second]};
+        //std::cout<<"thread start task" << std::endl;
+        if (q->have_tasks) {
+            std::pair<int, int> *task = q->pop();
+            if (not task)
+                continue;
             matrix result_matrix = matrix(shape, shape);
 
-            all_matrix_lock.unlock();
-            matrix *xm = data[task.first];
-            matrix *ym = data[task.second];
-            all_matrix_lock.unlock();
+            //all_matrix_lock.unlock();
+            matrix *xm = data[task->first];
+            matrix *ym = data[task->second];
+            //all_matrix_lock.unlock();
             for (size_t i = 0; i < shape; i++)
                 for (size_t j = 0; j < shape; j++) {
                     int c = 0;
-                    for (size_t g=0; g < shape; g++){
-                        int x = xm->data[i*shape + g];
-                        int y = ym->data[g*shape + j];
-                        c+=x*y ;
+                    for (size_t g = 0; g < shape; g++) {
+                        int x = xm->data[i * shape + g];
+                        int y = ym->data[g * shape + j];
+                        c += x * y;
                     }
-                    result_matrix.data[i*shape+j] = c;
+                    result_matrix.data[i * shape + j] = c;
                 }
             out_lock.lock();
             for (size_t i = 0; i < shape; i++)
                 for (size_t j = 0; j < shape; j++)
-                    out_matrix->data[i*shape+j] += result_matrix.data[i*shape+j];
+                    out_matrix->data[i * shape + j] += result_matrix.data[i * shape + j];
             out_lock.unlock();
-        }
-        else if (q->done)
+
+        } else if (q->done)
             return;
     }
 
@@ -91,7 +98,7 @@ void anyFunc(block_write_queue<std::pair<int,int>> * q, std::vector<matrix*> *da
 int main(int argc, char * argv[]) {
     int shape = 4;
     std::vector<matrix*> all_matrix;
-    block_write_queue<std::pair<int,int>> tasks;
+    block_write_queue tasks;
 
     if (argc != 3){
         std::cout << "mul thr_num file_name"<< std::endl;
@@ -119,9 +126,9 @@ int main(int argc, char * argv[]) {
 
 
         auto id = all_matrix.size();
-        all_matrix_lock.lock();
+        //all_matrix_lock.lock();
         all_matrix.push_back(A);
-        all_matrix_lock.unlock();
+        //all_matrix_lock.unlock();
         for (size_t i = 0; i < id; i++) {
             tasks.push(std::pair<int, int>(i, id));
             tasks.push(std::pair<int, int>(id, i));
